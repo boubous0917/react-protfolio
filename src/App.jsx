0b88b7c1b770fe49959f8405303dad941961eef5
boubous <Boubous@chaostw.com>
2024-08-28 18:48:27 +0800

    second

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import LoadingPage from "./pages/Loading";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
+// token -> 27 sep 2024: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
 export default function App() {
   // useGSAP(() => {
   //   let tl = gsap.timeline();
@@ -21,11 +21,11 @@ export default function App() {
   return (
     <>
       {/* <LoadingPage /> */}
-      <div className="relative overflow-x-hidden text-neutral-200 antialiased selection:bg-slate-600 selection:text-sky-100">
-        <div className="fixed top-0 -z-10 h-full w-full">
+      <div className="relative overflow-x-hidden antialiased text-neutral-200 selection:bg-slate-600 selection:text-sky-100">
+        <div className="fixed top-0 w-full h-full -z-10">
           <div className="absolute inset-0 -z-10 size-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]" />
         </div>
-        <div className="container mx-auto px-8">
+        <div className="container px-8 mx-auto">
           <Navbar />
           <Hero />
           <About />
