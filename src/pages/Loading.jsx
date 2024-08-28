200635ba9872a83f61bb30de26464f8dd3c9f151
boubous <Boubous@chaostw.com>
2024-08-28 20:46:09 +0800

    將gsap套件及相關程式移除

--- src/pages/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import gasp from "gasp";
-
-const LoadingPage = () => {
-  return (
-    <div className="wrapper absolute top-0 left-0 w-screen h-screen bg-black"></div>
-  );
-};
-
-export default LoadingPage;
