17887a2d1f8f51811193e150d657254ee4460506
boubous <Boubous@chaostw.com>
2024-08-28 21:23:11 +0800

    嘗試將套件移除

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import alias from "@rollup/plugin-alias";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [alias(), react()],
-  resolve: {
-    alias: {
-      "/@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
 });
